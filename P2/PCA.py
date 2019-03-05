fb514e2baaceed3347d63b20259fd4424b2ccf0f
carlesu <carles.ubach.upc@gmail.com>
2019-03-06 00:22:04 +0100

    Added a loop for printing, and some figures display.

--- P2/PCA.py
+++ P2/PCA.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 # E1
 #  T1
@@ -10,7 +11,9 @@ mu=[-1.2, 7.2]
 sigma=[0.1, 2.3]
 n=100000
 data2=pd.concat([pd.DataFrame(np.random.normal(mu[0], sigma[0], n), columns=['1']), pd.DataFrame(np.random.normal(mu[1], sigma[1], n), columns=['2'])], axis=1)
+plt.figure()
 sns.distplot(data2['1']) # We can see the distribution 1
+plt.figure()
 sns.distplot(data2['2']) # We can see the distribution 2
 sns.pairplot(data2) # Checking correlations on Distribution 1 vs Distribution 2
 
@@ -25,16 +28,12 @@ sns.pairplot(datatot) # Checking correlations on Distribution 1 vs Distribution 
 pca = PCA(n_components=4) # We do PCA on all variables, so we can decide after which ones we use
 pca.fit(datatot) # We do PCA of our whole data
 print('Explained Variance:')
-print('Dist 1:', pca.explained_variance_[0]) # We print our variance so we can have a 95 % of our ratio.
-print('Dist 2:', pca.explained_variance_[1])
-print('Dist 3:', pca.explained_variance_[2])
-print('Dist 4:', pca.explained_variance_[3])
-print('Explained Variance Ratio:')
-print('Dist 1:', pca.explained_variance_ratio_[0]) # We print our variance so we can have a 95 % of our ratio.
-print('Dist 2:', pca.explained_variance_ratio_[1])
-print('Dist 3:', pca.explained_variance_ratio_[2])
-print('Dist 4:', pca.explained_variance_ratio_[3])
-print('Singular Valures', pca.singular_values_)
+for i,b in enumerate(pca.explained_variance_):
+    print('Dist', i,':', b)
+print('\nExplained Variance Ratio:')
+for i,b in enumerate(pca.explained_variance_ratio_):
+    print('Dist', i,':', b)
+print('\nSingular Valures', pca.singular_values_)
 # Plot from eigenvalues needed, homework
 # T6
 # What?
